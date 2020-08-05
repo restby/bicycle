7f480e5be1dd1d5412094c62cf9e158765ccab1d
restby <restby@gmail.com>
2020-08-05 18:58:33 +0300

    many errors

---
+++ #src/js/_vendors.js
@@ -0,0 +1,5 @@
+// @@include('libs/_webp.min.js');
+// @@include('libs/_jquery-3.4.1.min.js');
+// @@include('libs/_slick.min.js');
+// @@include('libs/fl/_ibg.js');
+// @@include('libs/_picturefill.min.js');
---
+++ #src/js/script.js
@@ -0,0 +1,53 @@
+// @@include('_vendors.js');
+
+/*burger & navigation*/
+document.querySelector('.main-header__burger').addEventListener('click', function (e) {
+  e.preventDefault();
+  document.querySelector('.main-header__burger').classList.toggle('active');
+  document.querySelector('.main-header').classList.toggle('active');
+  document.querySelector('.main-header__navigation').classList.toggle('active');
+  document.querySelector('body').classList.toggle('lock');
+});
+$(function () {
+  /*slick-slider */
+  $('.slider').slick({
+    dots: true,
+    infinite: true,
+    speed: 600,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    arrows: false,
+    responsive: [
+      {
+        breakpoint: 1169.98,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 1,
+          infinite: true,
+          dots: true,
+          arrows: false
+        }
+      },
+      {
+        breakpoint: 839.98,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 1,
+          infinite: true,
+          dots: true,
+          arrows: false
+        }
+      },
+      {
+        breakpoint: 479.98,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+          infinite: true,
+          dots: true,
+          arrows: false
+        }
+      }
+    ]
+  });
+});
