de89fa633aee33e5ed3b475f466853e18dcccf46
restby <restby@gmail.com>
2023-08-07 13:42:04 +0300

    добавляет бургер-меню и навигацию

--- source/js/main.js
+++ source/js/main.js
@@ -1,6 +1,6 @@
 import {iosVhFix} from './utils/ios-vh-fix';
-import {initModals} from './modules/modals/init-modals';
 import {Form} from './modules/form-validate/form';
+import {Burger} from './modules/header/burger';
 
 // ---------------------------------
 
@@ -17,7 +17,8 @@ window.addEventListener('DOMContentLoaded', () => {
   // все скрипты должны быть в обработчике 'DOMContentLoaded', но не все в 'load'
   // в load следует добавить скрипты, не участвующие в работе первого экрана
   window.addEventListener('load', () => {
-    initModals();
+    const burger = new Burger();
+    burger.init();
     const form = new Form();
     window.form = form;
     form.init();
